22c12371d02c366f08ba4224c1e93b75bbe1208b
Dylan Swen <dylan.swen@gmail.com>
2016-04-19 21:20:38 -0400

    switch over enum

--- src/public/app/core/types/ScPlayer.ts
+++ src/public/app/core/types/ScPlayer.ts
@@ -8,21 +8,20 @@ export interface ScPlayer {
     on(event: ScPlayerEvent, handler: () => void ): void;
 }
 
-export enum ScPlayerEvent {
-    StateChange = <any>'state-change',
-    Play = <any>'play',
-    PlayStart = <any>'play-start',
-    PlayResume = <any>'play-resume',
-    Pause = <any>'pause',
-    Finished = <any>'finish',
-    Seek = <any>'seek',
-    Seeked = <any>'seeked',
-    GeoBlocked = <any>'geo_blocked',
-    BufferingStart = <any>'buffering_start',
-    BufferingEnd = <any>'buffering_end',
-    AudioError = <any>'audio_error',
-    Time = <any>'time',
-    NoStreams = <any>'no_streams',
-    NoProtocol = <any>'no_protocol',
-    NoConnection = <any>'no_connection'
-}
+export type ScPlayerEvent =
+    'state-change' |
+    'play' |
+    'play_start' |
+    'play_resume' |
+    'pause' |
+    'finish' |
+    'seek' |
+    'seeked' |
+    'geo_blocked' |
+    'buffering_start' |
+    'buffering_end' |
+    'audio_error' |
+    'time' |
+    'no_streams' |
+    'no_protocol' |
+    'no_connection';
